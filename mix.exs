f421d6addf95ce8ed7f3b47d8e4029f81247846f
solar05 <artem00298@gmail.com>
2020-05-14 18:25:36 +0300

    Add footer and fsm

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule ElixirSeo.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:machinery, "~> 1.0.0"}
     ]
   end
 
